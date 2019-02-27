8198a28beaa3480bd710f5306903672cdb989741
Antonio Jack <ict2jack@gmail.com>
2019-02-27 20:29:20 +0700

    Add sample data and change default pin icon.

--- src/App.js
+++ src/App.js
@@ -8,6 +8,18 @@ import './App.css';
 
 L.Icon.Default.imagePath =
   '//cdnjs.cloudflare.com/ajax/libs/leaflet/1.3.4/images/'
+export const busIcon = new L.Icon({
+  iconUrl: require('./assets/bus_line2.png'),
+  iconRetinaUrl: require('./assets/bus_line2.png'),
+  iconAnchor: [5, 55],
+  popupAnchor: [10, -44],
+  iconSize: [25, 28],
+  shadowUrl: './assets/marker-shadow.png',
+  shadowSize: [68, 95],
+  shadowAnchor: [20, 92],
+})
+
+const jsondata = require('./sample_data/20190121-1900.json');
 
 const position = [14.0712, 100.6135]
 const axios = require('axios');
@@ -19,11 +31,18 @@ class App extends Component {
 
   async componentWillMount() {
     try {
+      let index = 0;
       setInterval(async () => {
-        const apiResponse = await axios.get('http://localhost:3000/')
-        this.setState({ markerPos: [apiResponse.data[0].lat, apiResponse.data[0].lon] })
-        console.log(apiResponse)
-      }, 3000);
+        this.setState({ markerPos: [jsondata.data[index].lat, jsondata.data[index].lon] })
+        index++;
+        console.log('TCL: App -> componentWillMount -> index', index)
+
+        // const apiResponse = await axios.get('http://localhost:3001/')
+        // console.log(apiResponse)
+        // if (apiResponse.status === 200) {
+        //   this.setState({ markerPos: [apiResponse.data[0].lat, apiResponse.data[0].lon] })
+        // } 
+      }, 2000);
 
     }
     catch (error) {
@@ -39,7 +58,7 @@ class App extends Component {
             attribution='&amp;copy <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
             url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
           />
-          <Marker position={this.state.markerPos}>
+          <Marker position={this.state.markerPos} icon={busIcon}>
             <Popup>
               A pretty CSS3 popup. <br /> Easily customizable.
           </Popup>
