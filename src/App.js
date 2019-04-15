712ae8c43537ab3d37e03b0746a0b9da16f9030f
gis_bow <jack@ubuntu.ubuntu-domain>
2019-04-15 16:24:13 +0700

    Add bus route buttons

--- src/App.js
+++ src/App.js
@@ -2,22 +2,24 @@ import React, { useState, useEffect } from "react";
 import { get } from 'lodash'
 import moment from "moment";
 
-// core Leaflet component
-import { Map, Marker, Polyline, Popup, TileLayer } from 'react-leaflet';
-import L from 'leaflet';
-import 'leaflet/dist/leaflet.css'
 import './App.css';
 
+// core Leaflet component
+import { Map, Marker, Tooltip, TileLayer } from 'react-leaflet';
+import L from 'leaflet';
+import 'leaflet/dist/leaflet.css'
+import ControlBtn from './components/BusRoute'
+
+//Socket.io
 import io from 'socket.io-client';
 const socket = io('https://service.mappico.co.th');
 socket.emit('room', 'THAMMASAT');
 
+//Set image path for Leaflet
 L.Icon.Default.imagePath =
   '//cdnjs.cloudflare.com/ajax/libs/leaflet/1.3.4/images/';
 
-const busline = require('./assets/bus.json');
 const position = [14.07216624, 100.60579777];
-const bus2 = get(busline, ['line', '2', 'loc']).map(loc => [loc.latitude, loc.longitude]);
 
 function BusMarker({ idx, marker }) {
   const busMapping = require('./assets/busLineMatching.json')
@@ -36,14 +38,14 @@ function BusMarker({ idx, marker }) {
 
   return (
     <Marker key={idx} icon={icon} position={{ lat, lon }}>
-      <Popup>{marker.driver}</Popup>
+      <Tooltip> busNo: {marker.carno}  <br /> status: {marker.carstatus} <br /> speed: {marker.speed}  </Tooltip>
     </Marker>
   );
 }
 
 function App() {
   const [userLocation, setUserLocation] = useState(position);
-  const [busIds, setBusIds] = useState([]);
+  const [busIds, setBusIds] = useState(["228LE2018000993"]);
   const [markers, setMarkers] = useState([
     {
       id: "228LE2018000993",
@@ -78,31 +80,31 @@ function App() {
   ]);
 
   useEffect(() => {
-    // let item = {
-    //   id: "228LE2018000993",
-    //   lat: 14.07023055,
-    //   lon: 100.61050773,
-    //   timestamp: "2019-04-09T10:31:11.000Z",
-    //   acctime: "2019-04-09T10:27:52.000Z",
-    //   speed: 13.895999999999999,
-    //   direction: 358,
-    //   carno: "0TU0017",
-    //   icn: "bus_line2.png",
-    //   carstatus: "online",
-    //   company: "THAMMASAT",
-    //   driver: "Mr Aod Amornpak",
-    //   satellite: 11
-    // };
+    let item = {
+      id: "228LE2018000993",
+      lat: 14.07023055,
+      lon: 100.61050773,
+      timestamp: "2019-04-09T10:31:11.000Z",
+      acctime: "2019-04-09T10:27:52.000Z",
+      speed: 13.895999999999999,
+      direction: 358,
+      carno: "0TU0017",
+      icn: "bus_line2.png",
+      carstatus: "online",
+      company: "THAMMASAT",
+      driver: "Mr Aod Amornpak",
+      satellite: 11
+    };
 
-    // let busIndex = busIds.indexOf(item.id);
-    // if (busIndex === -1)
-    //   setMarkers([item])
-    // else {
-    //   const newMarkers = [...markers];
-    //   newMarkers.splice(busIndex, 1); //Delete
-    //   newMarkers.splice(busIndex, 0, item); //Insert
-    //   setMarkers(newMarkers)
-    // }
+    let busIndex = busIds.indexOf(item.id);
+    if (busIndex === -1)
+      setMarkers([item])
+    else {
+      const newMarkers = [...markers];
+      newMarkers.splice(busIndex, 1); //Delete
+      newMarkers.splice(busIndex, 0, item); //Insert
+      setMarkers(newMarkers)
+    }
   }, [])
 
   return (
@@ -112,7 +114,7 @@ function App() {
           attribution='&amp;copy <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
           url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
         />
-        <Polyline positions={bus2} color={'#5DBCD2'} weight={5} />
+        <ControlBtn />
         {markers.map((marker, index) => (
           <BusMarker
             key={index}
@@ -121,9 +123,7 @@ function App() {
           />
         ))}
         <Marker position={userLocation}>
-          <Popup>
-            You're here
-          </Popup>
+          <Tooltip sticky>You are here</Tooltip>
         </Marker>
       </Map>
     </div>
