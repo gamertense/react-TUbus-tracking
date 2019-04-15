a222f473d3fd39e0d07c0f30ef484b7b1d801981
gis_bow <jack@ubuntu.ubuntu-domain>
2019-04-15 17:07:19 +0700

    Show all routes at the beginning.

--- src/components/BusRoute.js
+++ src/components/BusRoute.js
@@ -5,13 +5,35 @@ import { Polyline } from 'react-leaflet'
 
 const busline = require('../assets/bus.json');
 
+const allRoutes = [
+    { path: get(busline, ['line', '1A', 'loc']).map(loc => [loc.latitude, loc.longitude]), color: get(busline, ['line', '1A', 'color']) },
+    { path: get(busline, ['line', '1B', 'loc']).map(loc => [loc.latitude, loc.longitude]), color: get(busline, ['line', '1B', 'color']) },
+    { path: get(busline, ['line', '2', 'loc']).map(loc => [loc.latitude, loc.longitude]), color: get(busline, ['line', '2', 'color']) },
+    { path: get(busline, ['line', '3', 'loc']).map(loc => [loc.latitude, loc.longitude]), color: get(busline, ['line', '3', 'color']) },
+    { path: get(busline, ['line', '4', 'loc']).map(loc => [loc.latitude, loc.longitude]), color: get(busline, ['line', '4', 'color']) }
+]
+
 function BusRoute() {
-    const [route, setRoute] = useState([]);
-    const [routeColor, setRouteColor] = useState('#5DBCD2');
+    const [routes, setRoutes] = useState(allRoutes);
+    const [lines, setLines] = useState(['1A', '1B', '2', '3', '4']);
 
     const selectBus = line => {
-        setRoute(get(busline, ['line', line, 'loc']).map(loc => [loc.latitude, loc.longitude]));
-        setRouteColor(get(busline, ['line', line, 'color']));
+        const lineIndex = lines.indexOf(line);
+        if (lineIndex === -1) {
+            // Push new route and line
+            const newRoute = [...routes, { path: get(busline, ['line', line, 'loc']).map(loc => [loc.latitude, loc.longitude]), color: get(busline, ['line', line, 'color']) }];
+            const newLine = [...lines, line];
+            setRoutes(newRoute);
+            setLines(newLine);
+        } else {
+            //Remove from routes and lines
+            const newRoute = [...routes];
+            const newLine = [...lines];
+            newRoute.splice(lineIndex, 1);
+            newLine.splice(lineIndex, 1);
+            setRoutes(newRoute)
+            setLines(newLine);
+        }
     }
 
     return (
@@ -23,7 +45,9 @@ function BusRoute() {
                 <button onClick={() => selectBus('3')}>3</button>
                 <button onClick={() => selectBus('4')}>4</button>
             </Control>
-            <Polyline positions={route} color={routeColor} weight={5} />
+            {routes.map((route, index) =>
+                <Polyline key={index} positions={route.path} color={route.color} weight={5} />
+            )}
         </div>
     )
 }
