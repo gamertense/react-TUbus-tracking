298539d7118a560cd0436c8398d5ac592cd7671a
jack <jack@ubuntu.ubuntu-domain>
2019-04-27 12:51:50 +0700

    Add marker rotation

--- src/components/BusMap/BusMap.js
+++ src/components/BusMap/BusMap.js
@@ -6,8 +6,9 @@ import './BusMap.css';
 // core Leaflet component
 import { Map, Marker, Tooltip, TileLayer } from 'react-leaflet';
 import L from 'leaflet';
-import 'leaflet/dist/leaflet.css'
 import ControlBtn from '../../components/BusRoute/BusRoute'
+import RotatedMarker from "./RotatedMarker";
+import 'leaflet/dist/leaflet.css'
 
 //Socket.io
 import io from 'socket.io-client';
@@ -21,14 +22,34 @@ L.Icon.Default.imagePath =
 
 const position = [14.07216624, 100.60579777];
 
+function DestinationMarker({ destination }) {
+    const icon = L.icon({
+        iconUrl: require(`../../assets/icon/destination.png`),
+        iconSize: [58, 65], // size of the icon
+        shadowSize: [50, 64], // size of the shadow
+        iconAnchor: [24, 55], // point of the icon which will correspond to marker's location
+        shadowAnchor: [4, 62],  // the same for the shadow
+        popupAnchor: [-3, -76] // point from which the popup should open relative to the iconAnchor
+    });
+    const lat = destination.lat;
+    const lon = destination.lon;
+
+    return (
+        <Marker icon={icon} position={destination}>
+            <Tooltip sticky>Your destination</Tooltip>
+        </Marker>
+    );
+}
+
 function BusMarker({ idx, marker }) {
     const busMapping = require('../../assets/busLineMatching.json')
     const busIconNumber = get(busMapping, marker.carno);
+    console.log(marker.direction)
 
     const icon = L.icon({
         iconUrl: require(`../../assets/icon/bus-line-${busIconNumber}.png`),
         popupAnchor: [10, -44],
-        iconSize: [25, 28],
+        iconSize: [30, 48],
         shadowUrl: '../../assets/marker-shadow.png',
         shadowSize: [68, 95],
         shadowAnchor: [20, 92],
@@ -37,12 +58,12 @@ function BusMarker({ idx, marker }) {
     const lon = marker.lon;
 
     return (
-        <Marker key={idx} icon={icon} position={{ lat, lon }}>
+        <RotatedMarker key={idx} icon={icon} position={{ lat, lon }} rotationAngle={marker.direction}>
             <Tooltip> Bus No: {marker.carno}  <br />
                 Status: {marker.carstatus} <br />
                 Speed: {marker.speed.toFixed(2)} km/hr <br />
                 Engine started: {moment(new Date(marker.acctime)).format("YYYY-MM-DD HH:mm")}</Tooltip>
-        </Marker>
+        </RotatedMarker>
     );
 }
 
@@ -62,6 +83,7 @@ export default function BusMap(props) {
             const splitted = props.origin.split(',');
             setUserLocation([parseFloat(splitted[0]), parseFloat(splitted[1])]);
         }
+        //If destination provided
         if (props.dest !== undefined) {
             const splitted = props.dest.split(',');
             setDestination([parseFloat(splitted[0]), parseFloat(splitted[1])]);
@@ -126,9 +148,8 @@ export default function BusMap(props) {
                 <Tooltip sticky>You are here</Tooltip>
             </Marker>
             {destination ?
-                <Marker position={destination}>
-                    <Tooltip sticky>Your destination</Tooltip>
-                </Marker> : null
+                <DestinationMarker destination={destination} />
+                : null
             }
         </Map>
     )
